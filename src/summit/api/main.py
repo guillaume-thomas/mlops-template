e45a02ed30cf624910c8a655a09da2c9c6743138
guillaume-thomas <guillaume.thomas@axa.fr>
2025-12-06 13:28:07 +0100

    WARNING : add trace log level in order to understand why otel does not work

--- src/summit/api/main.py
+++ src/summit/api/main.py
@@ -3,7 +3,7 @@ import uvicorn
 from summit.api import infer
 
 def main():
-    uvicorn.run(infer.app, host="0.0.0.0", port=8080)
+    uvicorn.run(infer.app, host="0.0.0.0", port=8080, log_level="trace")
 
 if __name__ == "__main__":
     main()
