87298bb55ca51f5456e423e2d3dab9ee7488c584
guillaume-thomas <guillaume.thomas@axa.fr>
2025-12-06 10:08:08 +0100

    feat: Bump to MlFlow 3.7 and add tracing and logging in Jaeger

--- scripts/install-mlflow.sh
+++ scripts/install-mlflow.sh
@@ -5,3 +5,4 @@ oc apply -f k8s/mlflow/mlflow.yml
 oc apply -f k8s/mlflow/dailyclean.yml
 oc label deployment dailyclean-api axa.com/dailyclean=false
 oc label statefulset mysql axa.com/dailyclean=true
+oc apply -f k8s/monitoring/jaeger.yaml
--- src/summit/api/infer.py
+++ src/summit/api/infer.py
@@ -1,11 +1,34 @@
+import os
 import pickle
 from dataclasses import dataclass
 from enum import Enum
 import pandas as pd
 
 from fastapi import FastAPI
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
+from opentelemetry.exporter.otlp.proto.http.trace_exporter import OTLPSpanExporter as HTTPSpanExporter
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.resources import Resource
+
+
+JAEGER_ENDPOINT = os.getenv("JAEGER_ENDPOINT", "http://jaeger.gthomas59800-dev.svc.cluster.local:4318/v1/traces")
+
+resource = Resource(attributes={
+    "service.name": "titanic-inference-api"
+})
+
+provider = TracerProvider(resource=resource)
+processor = BatchSpanProcessor(HTTPSpanExporter(endpoint=JAEGER_ENDPOINT))
+provider.add_span_processor(processor)
+trace.set_tracer_provider(provider)
+
+tracer = trace.get_tracer(__name__)
 
 app = FastAPI()
+FastAPIInstrumentor.instrument_app(app)
+
 model = pickle.load(open("./src/summit/api/resources/model.pkl", "rb"))
 
 
@@ -39,8 +62,19 @@ def health():
 
 @app.post("/infer")
 def infer(passenger: Passenger) -> list:
-    df_passenger = pd.DataFrame([passenger.to_dict()])
-    df_passenger["Sex"] = pd.Categorical(df_passenger["Sex"], categories=[Sex.FEMALE.value, Sex.MALE.value])
-    df_to_predict = pd.get_dummies(df_passenger)
-    res = model.predict(df_to_predict)
-    return res.tolist()
+    with tracer.start_as_current_span("model_inference") as span:
+        span.set_attribute("passenger.pclass", passenger.pclass.value)
+        span.set_attribute("passenger.sex", passenger.sex.value)
+        span.set_attribute("passenger.sibsp", passenger.sibSp)
+        span.set_attribute("passenger.parch", passenger.parch)
+
+        df_passenger = pd.DataFrame([passenger.to_dict()])
+        df_passenger["Sex"] = pd.Categorical(df_passenger["Sex"], categories=[Sex.FEMALE.value, Sex.MALE.value])
+        df_to_predict = pd.get_dummies(df_passenger)
+
+        res = model.predict(df_to_predict)
+
+        span.set_attribute("prediction.result", int(res[0]))
+        span.add_event("prediction_completed", {"result": int(res[0])})
+
+        return res.tolist()
